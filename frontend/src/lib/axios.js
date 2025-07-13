e3299c96fe1aef0a3345881fcac89a236ffb2bc9
--replace-all <surajprofile27@gmail.com>
2025-07-13 23:11:00 +0530

    added backend link

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URl =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://mern-thinkboard-api.vercel.app/api";
 
 const api = axios.create({
   baseURL: BASE_URl,
